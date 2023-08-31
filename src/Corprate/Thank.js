3d76c76f0fae45164f58403d5372545e89190345
guri-sachin <107688069+guri-sachin@users.noreply.github.com>
2023-08-31 14:49:27 +0530

    Add files via upload

---
+++ src/Corprate/Thank.js
@@ -0,0 +1,512 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Form, Button, Container } from 'react-bootstrap';
+import Swal from "sweetalert2";
+import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+import { Route, Link, Routes } from 'react-router-dom';
+import { fetch } from 'whatwg-fetch';
+import jquery from "jquery";
+import $ from "jquery";
+import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+// import './App.css';
+
+
+function Admin() {
+  const { user, loginWithRedirect } = useAuth0();
+  console.log("Current User", user);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [name, setName] = useState('');
+
+  const [product, setProduct] = useState(['']);
+
+  //   const [show, setShow] = useState('new');
+
+  const [sthjk, setSthjk] = useState('0');
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(location.state)
+      var email = location.state
+      try {
+
+        const response = await axios.post(`http://16.171.153.71:3306/corpstattru/${email}`);
+        const data = response.data;
+        console.log(data)
+        setProduct(data);
+
+        if (data.length > 0) {
+          setSthjk(data[0].status);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [location.state.email]);
+
+  console.log(sthjk)
+  //   useEffect(()=>{
+
+
+  //     var data =  email
+
+  //     axios.post(`http://16.171.153.71:3306/corpstattru/${data}`).then(
+  //     res =>setProduct(res.data));
+
+
+  // setSthjk(product[0].status)
+  //       },[product]);
+
+  console.log(product)
+
+  function handelDemo2(e) {
+    setName(e.target.value);
+
+  }
+
+  function Show(e) {
+
+    Swal.fire({
+      icon: 'success',
+      // title: 'Password Changed',
+      text: "Payment done Know you have access corprate dashboard ",
+    })
+    navigate('/Copdash')
+  }
+
+  return (
+    <div  >
+      <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css" />
+      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+      <link href="css/ruang-admin.min.css" rel="stylesheet"></link>
+
+      <div id="">
+
+
+
+        {/* <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+      
+        </div>
+        <div class="sidebar-brand-text mx-3">Nomad Admin</div>
+      </a> */}
+
+        {/* <li class="nav-item active">
+        <a class="nav-link" href="/Dasboard">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+      <hr class="sidebar-divider"/>
+        
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap"
+          aria-expanded="true" aria-controls="collapseBootstrap">
+          <i class="far fa-fw fa-user"></i>
+          <span>Users</span>
+        </a>
+        <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Users</h6>
+            <a class="collapse-item" href="Register">Registered Users</a>
+            <a class="collapse-item" href="Paid">Paid Users</a>
+            <a class="collapse-item" href="Subscribed">Subscribed Users</a>
+            <a class="collapse-item" href="Reported">Reported Users</a>
+          
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Subscriptions">
+          <i class="fas fa-fw fa-user-plus"></i>
+          <span>Subscriptions</span>
+        </a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="Categories">
+          <i class="fa fa-list-alt"></i>
+          <span>Categories</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Subcategore">
+          <i class="fa fa-list-alt"></i>
+          <span>Sub Categories</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Content1">
+          <i class="fa fa-file"></i>
+          <span>Manage Content</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Content">
+          <i class="fa fa-newspaper"></i>
+          <span>Analyse Content Trend</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Message">
+          <i class="fa fa-envelope"></i>
+          <span>Inspirational Message</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Social">
+          <img src="img/social.jpg"/>
+          <span>Social Media Groups</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Video">
+         <i class="fa fa-play-circle" ></i>
+          <span>Dance Videos</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Live">
+         <i class="fa fa-stream" ></i>
+          <span>Live Sessions</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap1"
+          aria-expanded="true" aria-controls="collapseBootstrap1">
+          <i class="fa fa-building"></i>
+          <span>Corporate Users</span>
+        </a>
+        <div id="collapseBootstrap1" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+          <a class="collapse-item" href="Corporate1">Corporate Users</a>
+            <a class="collapse-item" href="Corporateaccept">Accpted User</a>
+            <a class="collapse-item" href="Rejectcorporate">Rejected User</a>
+            <a class="collapse-item" href="Invoicelist">Invoice List</a>
+           
+           
+          
+          </div>
+        </div>
+      </li>
+      
+            <li class="nav-item">
+        <a class="nav-link" href="Transaction">
+         <i class="fa fa-credit-card" ></i>
+          <span>Transaction History</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Ticket">
+         <i class="fas fa-ticket-alt" ></i>
+          <span>Ticket Raised</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Enquiry">
+         <i class="fa fa-question-circle" ></i>
+          <span>Inquiry Form</span>
+        </a>
+      </li>
+       */}
+
+
+
+
+
+
+
+        <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content">
+
+            <nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top">
+              {/* <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button> */}
+              <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown no-arrow">
+                  <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" data-target="#colk"
+                    aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-search fa-fw"></i>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                    aria-labelledby="searchDropdown" id='colk'>
+                    <form class="navbar-search">
+                      <div class="input-group">
+                        <input type="text" class="form-control bg-light border-1 small" placeholder="What do you want to look for?"
+                          aria-label="Search" aria-describedby="basic-addon2" style={{ border_color: "#3f51b5;" }} />
+                        <div class="input-group-append">
+                          <button class="btn btn-primary" type="button">
+                            <i class="fas fa-search fa-sm"></i>
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </li>
+
+
+                <div class="topbar-divider d-none d-sm-block"></div>
+                <li class="nav-item dropdown no-arrow">
+                  <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" data-target="#wetrg"
+                    aria-haspopup="true" aria-expanded="false">
+                    <img class="img-profile rounded-circle" src="img/boy.png" />
+                    <span class="ml-2 d-none d-lg-inline text-white small">Corporate</span>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown" id='wetrg'>
+                    <a class="dropdown-item" href="#">
+                      <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                      Profile
+                    </a>
+                    {/* <a class="dropdown-item" href="#">
+                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Settings
+                </a>
+                <a class="dropdown-item" href="#">
+                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Activity Log
+                </a> */}
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="javascript:void(0);" data-toggle="modal" data-target="#logoutModal">
+                      <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                      Logout
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </nav>
+            {sthjk == 0 ?
+              <div class="jumbotron text-center" style={{ height: "500px" }}>
+                <h1 class="display-3">Thank You!</h1>
+                <p class="lead"><strong>Please check your email</strong> and have patient we are review your profile.</p>
+                <hr />
+                <p>
+                  Having trouble? <a href="https://url.com/">Contact us</a>
+                </p>
+                <p class="lead">
+                  <a class="btn btn-primary btn-sm" href="https://bootstrapcreative.com/" role="button">Continue to homepage</a>
+                </p>
+              </div>
+              :
+
+
+              <div class="">
+                <div class="container">
+                  <br />
+                  <hr />
+
+                  <div class="row">
+                    <aside class="col-sm-12">
+                      <p>Payment form</p>
+
+                      <article class="card">
+                        <div class="card-body p-5">
+                          <p> <a class="nav-link active" data-toggle="pill" href="#nav-tab-card">
+                            <i class="fa fa-credit-card"></i>Card Details</a>
+                          </p>
+                          <p class="alert alert-success">Nomad Zindagi Corporate Payment</p>
+
+                          <form role="form">
+                            <div class="form-group">
+                              <label for="username">Full name (on the card)</label>
+                              <div class="input-group">
+                                <div class="input-group-prepend">
+                                  <span class="input-group-text"><i class="fa fa-user"></i></span>
+                                </div>
+                                <input type="text" class="form-control" name="username" placeholder="" required="" />
+                              </div>
+                            </div>
+
+                            <div class="form-group">
+                              <label for="cardNumber">Card number</label>
+                              <div class="input-group">
+                                <div class="input-group-prepend">
+                                  <span class="input-group-text"><i class="fa fa-credit-card"></i></span>
+                                </div>
+                                <input type="text" class="form-control" name="cardNumber" placeholder="" />
+                              </div>
+                            </div>
+
+                            <div class="row">
+                              <div class="col-sm-8">
+                                <div class="form-group">
+                                  <label><span class="hidden-xs">Expiration</span> </label>
+                                  <div class="form-inline">
+                                    <select class="form-control" style={{ width: "45%" }}>
+                                      <option>MM</option>
+                                      <option>01 - Janiary</option>
+                                      <option>02 - February</option>
+                                      <option>03 - February</option>
+                                    </select>
+                                    <span style={{ width: "10%", textAlign: "center" }}> / </span>
+                                    <select class="form-control" style={{ width: "45%" }}>
+                                      <option>YY</option>
+                                      <option>2018</option>
+                                      <option>2019</option>
+                                    </select>
+                                  </div>
+                                </div>
+                              </div>
+                              <div class="col-sm-4">
+                                <div class="form-group">
+                                  <label data-toggle="tooltip" title="" data-original-title="3 digits code on back side of the card">CVV <i class="fa fa-question-circle"></i></label>
+                                  <input class="form-control" required="" type="text" />
+                                </div>
+                              </div>
+                            </div>
+                            <button class="subscribe btn btn-primary btn-block" type="button" onClick={Show}> Confirm  </button>
+                          </form>
+                        </div>
+                      </article>
+
+
+                    </aside>
+                    {/* <aside class="col-sm-6">
+<p>Paymetn form2</p>
+
+<article class="card">
+<div class="card-body p-5">
+
+<ul class="nav bg-light nav-pills rounded nav-fill mb-3" role="tablist">
+	<li class="nav-item">
+		<a class="nav-link active" data-toggle="pill" href="#nav-tab-card">
+		<i class="fa fa-credit-card"></i> Credit Card</a></li>
+	<li class="nav-item">
+		<a class="nav-link" data-toggle="pill" href="#nav-tab-paypal">
+		<i class="fab fa-paypal"></i>  Paypal</a></li>
+	<li class="nav-item">
+		<a class="nav-link" data-toggle="pill" href="#nav-tab-bank">
+		<i class="fa fa-university"></i>  Bank Transfer</a></li>
+</ul>
+
+<div class="tab-content">
+<div class="tab-pane fade show active" id="nav-tab-card">
+	<p class="alert alert-success">Some text success or error</p>
+	<form role="form">
+	<div class="form-group">
+		<label for="username">Full name (on the card)</label>
+		<input type="text" class="form-control" name="username" placeholder="" required=""/>
+	</div> 
+
+	<div class="form-group">
+		<label for="cardNumber">Card number</label>
+		<div class="input-group">
+			<input type="text" class="form-control" name="cardNumber" placeholder=""/>
+			<div class="input-group-append">
+				<span class="input-group-text text-muted">
+					<i class="fab fa-cc-visa"></i>   <i class="fab fa-cc-amex"></i>   
+					<i class="fab fa-cc-mastercard"></i> 
+				</span>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+	    <div class="col-sm-8">
+	        <div class="form-group">
+	            <label><span class="hidden-xs">Expiration</span> </label>
+	        	<div class="input-group">
+	        		<input type="number" class="form-control" placeholder="MM" name=""/>
+		            <input type="number" class="form-control" placeholder="YY" name=""/>
+	        	</div>
+	        </div>
+	    </div>
+	    <div class="col-sm-4">
+	        <div class="form-group">
+	            <label data-toggle="tooltip" title="" data-original-title="3 digits code on back side of the card">CVV <i class="fa fa-question-circle"></i></label>
+	            <input type="number" class="form-control" required=""/>
+	        </div> 
+	    </div>
+	</div> 
+	<button class="subscribe btn btn-primary btn-block" type="button"> Confirm  </button>
+	</form>
+</div> 
+<div class="tab-pane fade" id="nav-tab-paypal">
+<p>Paypal is easiest way to pay online</p>
+<p>
+<button type="button" class="btn btn-primary"> <i class="fab fa-paypal"></i> Log in my Paypal </button>
+</p>
+<p><strong>Note:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. </p>
+</div>
+<div class="tab-pane fade" id="nav-tab-bank">
+<p>Bank accaunt details</p>
+<dl class="param">
+  <dt>BANK: </dt>
+  <dd> THE WORLD BANK</dd>
+</dl>
+<dl class="param">
+  <dt>Accaunt number: </dt>
+  <dd> 12345678912345</dd>
+</dl>
+<dl class="param">
+  <dt>IBAN: </dt>
+  <dd> 123456789</dd>
+</dl>
+<p><strong>Note:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. </p>
+</div> 
+</div>
+
+</div> 
+</article> 
+
+
+	</aside> */}
+                  </div>
+
+                </div>
+
+                <br />
+                {/* <article class="bg-secondary mb-3">  
+<div class="card-body text-center">
+
+<p class="h5 text-white">Components and templates  <br/> for Ecommerce, marketplace, booking websites 
+and product landing pages</p>   <br/>
+<p><a class="btn btn-warning" target="_blank" href="/"> Bootstrap.com  
+ <i class="fa fa-window-restore "></i></a></p>
+</div>
+<br/>
+</article> */}
+              </div>
+
+            }
+
+
+
+
+          </div>
+
+          <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+              <div class="copyright text-center my-auto">
+                <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
+                  <b><a href="https://omkatech.com/" target="_blank">Omka Tech LLP</a></b>
+                </span>
+              </div>
+            </div>
+
+
+          </footer>
+
+        </div>
+      </div>
+
+
+      <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+      </a>
+
+
+
+
+    </div>
+
+  );
+
+}
+export default Admin;
